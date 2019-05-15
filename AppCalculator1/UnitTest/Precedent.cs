fb84422a15e69022aca0a94ab9909ab795957e94
Student023 <cool.1397@mail.ru>
2019-05-15 08:55:58 +0300

    Создан интерфейс программы

---
+++ AppCalculator1/UnitTest/Precedent.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppCalculator;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class Precedent
+    {/*
+        // Тест для Precedent1: 5/1 + 2/1 = 7/1
+        [TestMethod]
+        public void Precedent1()
+        {
+            Frac expected = new Frac("7/1");
+            Frac target = new Frac("5/1");
+            Frac Value = new Frac("2/1");
+            Frac actual;
+            actual = (Frac)target.Add(Value);
+            Assert.AreEqual(expected.GetNumberString(), actual.GetNumberString());
+        }
+
+        // Тест для Precedent2: 5/1 * = 25/1
+        [TestMethod]
+        public void Precedent2()
+        {
+            Frac expected = new Frac("25/1");
+            Frac target = new Frac("5/1");
+            Frac actual;
+            actual = (Frac)target.Mul();
+            Assert.AreEqual(expected.GetNumberString(), actual.GetNumberString());
+        }
+
+        // Тест для Precedent3: 5/1 + 4/1 = 9/1 = 13/1 = 17/1
+        [TestMethod]
+        public void Precedent3()
+        {
+            Frac expected = new Frac("17/1");
+            Frac target = new Frac("5/1");
+            Frac Value = new Frac("4/1");
+            Frac actual;
+            actual = (Frac)target.Add(Value);
+            Assert.AreEqual(expected.GetNumberString(), actual.GetNumberString());
+        }
+
+        // Тест для Precedent4: 2/1 + 3/1 = 5/1 = 8/1 + = 16/1
+        [TestMethod]
+        public void Precedent4()
+        {
+        }
+
+        // Тест для Precedent5: 5/1 «Sqr» 25/1
+        [TestMethod]
+        public void Precedent5()
+        {
+        }
+
+        // Тест для Precedent6: 6/1	«Sqr» + 2/1 «Sqr» / 10/1 + 6/1 = 10/1
+        [TestMethod]
+        public void Precedent6()
+        {
+        }
+*/
+    }
+}
